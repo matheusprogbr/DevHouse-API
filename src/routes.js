cde7ac4f5c350737f6e3e32f1e261f2df75ba762
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-26 22:31:51 -0300

    feat: Add reserve house feature

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import uploadConfig from "./config/upload";
 import SessionController from "./controllers/SessionController";
 import HouseController from "./controllers/HouseController";
 import DashboardController from "./controllers/DashboardController";
+import ReserveController from "./controllers/ReserveController";
+
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
@@ -15,6 +17,10 @@ routes.put("/houses/:id", upload.single("thumbnail") ,HouseController.update);
 routes.delete("/houses/:id", HouseController.delete);
 
 routes.get("/dashboard", DashboardController.show);
+
+routes.get("/reserves", ReserveController.index);
+routes.post("/houses/:id/reserve", ReserveController.store);
+routes.delete("/reserves", ReserveController.delete);
 
 
 export default routes;
